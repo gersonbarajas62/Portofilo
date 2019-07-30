b105896314667fffa0923774f0482201c14e8606
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-07-30 12:18:35 -0500

    changes added to files and created component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const About = () => (
-    <h1>SUP foo</h1>
+    <div className="About">
+        <div className="About-container">
+            <div className="About-avatar">
+                <figure>
+                    <img src="" alt="" />
+                </figure>
+            </div>
+            <div className="About-name">
+                <h2>Gerson Roa</h2>
+            </div>
+            <div className="About-profession">
+                <p> Fullstack developer</p>
+            </div>
+            <div className="About-desc">
+                <p>im a great coder</p>
+            </div>
+            <div className="About-location">
+                <p>Mexico city</p>
+            </div>
+
+            <div className="About-social">
+
+            </div>
+        </div>
+    </div>
 );
 
 export default About;
---
+++ src/components/Education.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Education = () => (
+    <div className="Education">
+        <div className="Education-container">
+            <div className="Education-item">
+                <h3> holacode</h3>
+                <p>javascript</p>
+            </div>
+
+        </div>
+
+    </div>
+)
+
+export default Education
